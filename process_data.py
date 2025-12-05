3aa6537dc9f811d4cce315944288a75fb4eb4328
seungwoo900 <danielee982@gmail.com>
2025-12-04 23:22:18 -0500

    move standardize_position and map_team functions to process_data

--- process_data.py
+++ process_data.py
@@ -1,15 +1,15 @@
 from rapidfuzz import fuzz, process
 import re
+import pandas as pd
 
-def dedup_high_schools(players_df, highschools_df):
-    # canonical_hs = schools_df[schools_df['school type'] == 'high school']['name'].unique()
-    canonical_hs = highschools_df['name'].unique()
+def dedup_last_schools(players_df, lastschools_df):
+    canonical_hs = lastschools_df['name'].unique()
 
-    hs_names = players_df['High School'].unique()
+    ls_names = players_df['Last School'].unique()
 
-    hs_mapping = {}
+    ls_mapping = {}
 
-    for school in hs_names:
+    for school in ls_names:
         matches = process.extract(
             school,
             canonical_hs,
@@ -17,26 +17,26 @@ def dedup_high_schools(players_df, highschools_df):
             score_cutoff=70,
         )
         if matches:
-            hs_mapping[school] = matches[0][0]
+            ls_mapping[school] = matches[0][0]
         else:
-            hs_mapping[school] = school
+            ls_mapping[school] = school
 
-    players_df['High School'] = players_df['High School'].map(hs_mapping).fillna(players_df['High School'])
-    highschools_df['name'] = highschools_df['name'].map(hs_mapping).fillna(highschools_df['name'])
+    players_df['Last School'] = players_df['Last School'].map(ls_mapping).fillna(players_df['Last School'])
+    lastschools_df['name'] = lastschools_df['name'].map(ls_mapping).fillna(lastschools_df['name'])
     
-    highschools_df.drop_duplicates(subset=['name'], inplace=True)
+    lastschools_df.drop_duplicates(subset=['name'], inplace=True)
 
-    print(f"Original: {len(hs_names)}")
-    print(f"Canonical: {len(set(hs_mapping.values()))}")
-    print(f"After deduplication: {len(hs_names) - len(set(hs_mapping.values()))} duplicates removed.")
+    print(f"Original: {len(ls_names)}")
+    print(f"Canonical: {len(set(ls_mapping.values()))}")
+    print(f"After deduplication: {len(ls_names) - len(set(ls_mapping.values()))} duplicates removed.")
 
-    return players_df, highschools_df
+    return players_df, lastschools_df
 
 def dedup_coaches(coaches_df):
     # 1. John Smith at ABC Univ: Head Coach
     # 2. John Smith at ABC Univ: Recruiting Coordinator
     # => 1. John Smith at ABC Univ: Head Coach | Recruiting Coordinator
-    coaches_clean = coaches_df.groupby(['Name', 'School'], as_index=False).agg({
+    coaches_clean = coaches_df.groupby(['Name', 'College'], as_index=False).agg({
         'Title': lambda x: ' | '.join(sorted(set(x)))
     })
     return coaches_clean
@@ -70,3 +70,46 @@ def standardize_positions(players_df):
         )
 
     return players_df
+
+def map_team(players_df, coaches_df):
+    teams_mapping = {
+        'University of Florida': 'Florida Gators',
+        'University of Missouri': 'Missouri Tigers',
+        'University of Oklahoma': 'Oklahoma Sooners',
+        'University of Alabama': 'Alabama Crimson Tide',
+        'University of Washington': 'Washington Huskies',
+        'University of Oregon': 'Oregon Ducks',
+        'University of Indiana': 'Indiana Hoosiers',
+        'University of Minnesota': 'Minnesota Golden Gophers',
+        'Texas A&M University': 'Texas A&M Aggies',
+        'University of Southern Mississippi': 'Southern Miss Golden Eagles',
+        'Troy University': 'Troy Trojans',
+        'University of Louisiana at Lafayette': 'Louisiana Ragin\' Cajuns',
+        'Rice University': 'Rice Owls',
+        'University of Memphis': 'Memphis Tigers',
+        'University of North Carolina at Charlotte': 'Charlotte 49ers',
+        'Oregon State University': 'Oregon State Beavers',
+        'Texas Tech University': 'Texas Tech Red Raiders',
+        'Oklahoma State University': 'Oklahoma State Cowboys',
+        'Fresno State University': 'Fresno State Bulldogs',
+        'Air Force Academy': 'Air Force Falcons',
+    }
+
+    players_df['Team'] = players_df['College'].apply(lambda x: teams_mapping.get(x))
+    coaches_df['Team'] = coaches_df['College'].apply(lambda x: teams_mapping.get(x))
+
+    return players_df, coaches_df
+
+if __name__ == '__main__':
+    players_df = pd.read_csv('data/raw/players.csv')
+    lastschools_df = pd.read_csv('data/raw/lastschools.csv')
+    coaches_df = pd.read_csv('data/raw/coaches.csv')
+
+    players_df, coaches_df = map_team(players_df, coaches_df)
+    coaches_df = dedup_coaches(coaches_df)
+    players_df, lastschools_df = dedup_last_schools(players_df, lastschools_df)
+    players_df = standardize_positions(players_df)
+
+    players_df.to_csv('data/processed/players.csv', index=False)
+    lastschools_df.to_csv('data/processed/lastschools.csv', index=False)
+    coaches_df.to_csv('data/processed/coaches.csv', index=False)
